2f798354e70c42d26ccb5efc9892fdd4a0d299c0
edu <bfwaend@gmail.com>
2025-01-12 14:21:59 -0300

    Finish ex03

--- ex03/ex03.rb
+++ ex03/ex03.rb
@@ -3,11 +3,11 @@
 class Elem
   attr_reader :tag, :content, :opt, :tag_type
 
-  def initialize(*args)
-    @tag = args[0]
-    @tag_type = ''
-    @content = []
-    @opt = {}
+  def initialize(tag, content = [], tag_type = 'double', opt = {})
+    @tag = tag
+    @tag_type = tag_type
+    @content = content
+    @opt = opt
   end
 
   def add_content(*args)
@@ -15,9 +15,40 @@ class Elem
       @content << arg
     end
   end
+
+  def to_s
+    "\"" + stringify[0...-2] + "\""
+  end
+
+  def stringify(content = "")
+    options = @opt.empty? ? '' : ' ' + @opt.map { |k, v| "#{k}='#{v}'" }.join(' ')
+
+    if @tag_type == 'double' && @content.is_a?(Array)
+      "<#{@tag}#{options}>\\n#{@content.flatten.map {|elem| elem.stringify}.join}</#{@tag}>\\n"
+    elsif @tag_type == 'double' && @content.is_a?(Text)
+      "<#{@tag}#{options}>#{@content.to_s}</#{@tag}>\\n"
+    else
+      "<#{@tag}#{options} />\\n"
+    end
+  end
 end
 
 class Text
-  def initialize(args)
+  def initialize(str)
+    @str = str
   end
+
+  def to_s
+    @str
+  end
+end
+
+if $PROGRAM_NAME == __FILE__
+  html = Elem.new('html')
+  head = Elem.new('head')
+  body = Elem.new('body')
+  title = Elem.new('title', Text.new('blah blah'))
+  head.add_content(title)
+  html.add_content([head, title, body])
+  puts html
 end
